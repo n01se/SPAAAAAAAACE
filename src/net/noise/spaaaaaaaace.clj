c98ca6043e02c2901646b0628fdd2d33ff467329
Aaron Brooks <aaron@brooks1.net>
2015-01-21 17:58:56 -0500

    Initial commit with gorilla repl.

---
+++ src/net/noise/spaaaaaaaace.clj
@@ -0,0 +1,6 @@
+(ns net.noise.spaaaaaaaace)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/net/noise/spaaaaaaaace_test.clj
@@ -0,0 +1,7 @@
+(ns net.noise.spaaaaaaaace-test
+  (:require [clojure.test :refer :all]
+            [net.noise.spaaaaaaaace :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
